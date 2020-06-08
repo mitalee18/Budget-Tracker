f1d3df5c03f04199b269ad428d91dc0926b46b8e
Mitalee Minde <mitaleeminde@gmail.com>
2020-06-08 11:50:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Budget Tracker
+
+Helps users to keep track of their expenses and manage their budget.
+
+## This is a coursework project
+
+[Udemy Javascript Course](https://www.udemy.com/course/the-complete-javascript-course/) 
---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+//BUDGET CONTROLLER
+var budgetController = (function(){
+	
+	//some code
+
+})();
+
+
+//UI Controller
+var UIController = (function (){
+
+	//some code
+
+})();
+
+
+//Global App Controller
+var controller = (function(budgetCtrl, UICtrl){
+
+	// some code
+
+})(budgetController, UIController);
